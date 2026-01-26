4d33b5c04c84538d8070bff14383f34458df9802
hctel <22167@ecam.be>
2026-01-26 13:39:44 +0100

    Create GameAchievement interface

---
+++ src/main/java/be/hctel/renaissance/global/IGameAchievement.java
@@ -0,0 +1,8 @@
+package be.hctel.renaissance.global;
+
+public interface IGameAchievement {
+	public String getName();
+	public String getDescription();
+	public String getStorageCode();
+	public int getUnlockLevel();
+}
